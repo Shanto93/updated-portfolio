2dfeea89a0828f81ffe6784e5999b2a619ade149
Shanto93 <shantoislam7363@gmail.com>
2025-01-03 21:50:47 +0600

    about page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,12 @@
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div>
+      {/* Title Box */}
+      <div className="border-t-2 w-96  border-red-500">
+        <h1 className="border p-4 w-fit relative -top-5">About Me</h1>
+      </div>
+    </div>
+  );
 };
 
 export default About;
